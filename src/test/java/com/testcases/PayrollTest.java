190e6f91f7356cb13bdda9788b8b7ef6d0824534
jayesh200026 <jayeshkumar8660@gmail.com>
2021-10-04 19:55:11 +0530

    Added feature to retieve data from database

--- src/test/java/com/testcases/PayrollTest.java
+++ src/test/java/com/testcases/PayrollTest.java
@@ -9,16 +9,18 @@ public class PayrollTest {
 
 	@Test
 	public void whenConnectedToDatabase_withCorrectCredentials_returnsTrue() {
-		EmployeePayroll emp = new EmployeePayroll("employe_payroll");
+		EmployeePayroll emp = new EmployeePayroll();
 		boolean result = emp.getConnection();
 		Assert.assertTrue("true", result);
 	}
-
+	
 	@Test
-	public void whenConnectedToDatabase_withInCorrectCredentials_returnsTrue() {
-		EmployeePayroll emp = new EmployeePayroll("employepayroll");
-		boolean result = emp.getConnection();
-		Assert.assertFalse("false", result);
+	public void whenConnectedToDatabase_forretreivingData_returnsTrue() {
+		EmployeePayroll emp = new EmployeePayroll();
+		boolean result = emp.selectData();
+		Assert.assertTrue("true", result);
 	}
+
+	
 
 }
